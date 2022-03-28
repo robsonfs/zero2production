5aa6b957893ec9a381c2ccef6f300600b025c5a2
Robson Santos <robfds@gmail.com>
2022-03-28 05:38:48 -0300

    Fixing a broken build

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,12 +20,11 @@ async fn health_check_works() {
 
 // Launch our application in the background
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
-    return format!("http://127.0.0.1:{port}")
+    format!("http://127.0.0.1:{}", port)
 }
