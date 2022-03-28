feedfed358520332c4523443de38b5cae5c4cc75
Robson Santos <robfds@gmail.com>
2022-03-27 22:14:39 -0300

    Organizing project

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8001")?
+    .run()
+    .await
+}
