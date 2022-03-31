d1a23f558ad267be55d2e8aa443d125b1277d1d5
Robson Santos <robfds@gmail.com>
2022-03-30 21:34:47 -0300

    Fixing a broken build: Forgot to apply cargo fmt changes.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
@@ -13,13 +13,11 @@ async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let mut configuration = get_configuration()
-        .expect("Failed to read configuration");
+    let mut configuration = get_configuration().expect("Failed to read configuration");
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
     let server =
-        zero2prod::startup::run(listener, connection_pool.clone())
-        .expect("Failed to bind address");
+        zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
 
@@ -31,15 +29,13 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create Database
-    let mut connection =
-        PgConnection::connect(&config.connection_string_without_db())
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
         .await
         .expect("Failed to connect Postgres");
-    connection.execute(
-        format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str()
-    )
-    .await
-    .expect("Failed to create database");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database");
 
     // Migrate Database
     let connection_pool = PgPool::connect(&config.connection_string())
@@ -50,7 +46,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to migrate the database");
 
-        connection_pool
+    connection_pool
 }
 
 #[tokio::test]
