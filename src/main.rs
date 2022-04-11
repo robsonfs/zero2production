a87cb3609d5f427332555f9a3a073337f310ff73
Robson Santos <robfds@gmail.com>
2022-04-11 10:03:13 -0300

    Small improvement in the database connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -13,9 +13,10 @@ async fn main() -> std::io::Result<()> {
 
     // Getting configs and establishing a DB connection
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
 
     // Starting an HTTP server
     let address = format!(
